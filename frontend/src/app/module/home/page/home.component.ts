a9c2d0e5ac5f26c255ca79ce39991a1483672f75
Habakuk90 <a.krahl90@gmail.com>
2020-06-25 21:23:11 +0200

    cleaned up homepage; adde svg examples

--- frontend/src/app/module/home/page/home.component.ts
+++ frontend/src/app/module/home/page/home.component.ts
@@ -1,35 +1,35 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, AfterViewInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { take, map } from 'rxjs/operators';
 import { ActivatedRoute } from '@angular/router';
 
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit, OnDestroy {
+export class HomeComponent implements OnInit, OnDestroy, AfterViewInit {
   title: string;
-  // protected slug$: Observable<string>;
-
-  constructor(
-    private route: ActivatedRoute) {
-  }
+  house = {
+    url: '../../../../assets/svg/house_full.svg',
+    zoom: false
+  };
+  constructor() {}
 
   ngOnInit() {
-    // this.slug$ = this.route.paramMap.pipe(map(params => (params.get('slug'))));
-    // this.slug$.pipe(take(1)).subscribe(slug => this.get());
-  }
-
-  get(): void {
-    const that = this;
   }
 
   ngOnDestroy() {
-    // fix me, have to call it in every HubComponent
+    // fix me, have to call it in every HubCompone
   }
 
-  registerOnMethods() {
+
+  ngAfterViewInit() {
+
+    setTimeout(() => {
+      this.house.zoom = true;
+      
+    }, 200);
+
   }
 }
