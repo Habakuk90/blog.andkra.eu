d0eb93e3d55c752d10d60123cf55e08c1aa22b91
Habakuk90 <a.krahl90@gmail.com>
2020-07-02 11:04:35 +0200

    fixed release issues

--- scripts/redeploy-docker.sh
+++ scripts/redeploy-docker.sh
@@ -1,9 +1,9 @@
 echo "removing old docker app"
-docker-compose down
+docker-compose -f ./docker/docker-compose.production.yml down
 echo "pulling latest..."
 docker pull wahnsinnshub/blog.andkra.eu.frontend:latest
 docker pull wahnsinnshub/blog.andkra.eu.backend:latest
 
 echo "docker compose up"
-docker-compose -f docker-compose.production.yml up -d --no-build
+docker-compose -f ./docker/docker-compose.production.yml up -d --no-build
 
